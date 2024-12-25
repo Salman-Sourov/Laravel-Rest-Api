854f89ae62452f7a6afdbe2a5a2aeca7a70047de
Salman-Sourov <salman.sourov40@gmail.com>
2024-12-26 01:26:08 +0600

    Applied CRUD Operation, Tested Via Postman

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -53,4 +53,53 @@ class StudentController extends Controller
             return response()->json($data, 200);
         }
     }
+
+    public function edit(Request $request){
+
+        $validator = Validator::make(
+            $request->all(),[
+                'name'=> "required",
+                'email'=>"required|email"
+            ]);
+
+        if($validator->fails()){
+            $data = [
+                'status' => 422,
+                'message' => $validator->messages()
+            ];
+
+            return response()->json($data,422);
+        }
+
+        else{
+
+            $student = Student::findOrFail($request->id);
+
+            $student->name = $request->name;
+            $student->email = $request->email;
+            $student->phone = $request->phone;
+
+            $student->save();
+
+            $data=[
+                'status' => 200,
+                'message' => 'Student Updated Successfully'
+            ];
+
+            return response()->json($data,200);
+        }
+    }
+
+    public function delete($id){
+
+        $student = Student::findOrFail($id);
+        $student->delete();
+
+        $data=[
+            'status' => 200,
+            'message' => 'Student Deleted Successfully'
+        ];
+
+        return response()->json($data,200);
+    }
 }
